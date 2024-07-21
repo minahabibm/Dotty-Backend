9f77762e6dce4333c3b7cd107559ec34741186e7
Mina Habib <mhcsc01@gmail.com>
2024-07-21 01:15:26 -0400

    Refactor - utils.

---
+++ src/main/java/com/tradingbot/dotty/utils/ExternalAPi/Auth0Util.java
@@ -0,0 +1,104 @@
+package com.tradingbot.dotty.utils.ExternalAPi;
+
+import com.tradingbot.dotty.exceptions.Auth0Exceptions;
+import com.tradingbot.dotty.models.dto.requests.AccessTokenAudAndJti;
+import com.tradingbot.dotty.models.dto.requests.AccessTokenResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import static com.tradingbot.dotty.utils.constants.LoggingConstants.EXTERNAL_GET_REQUEST_WITH_CRITERIA;
+
+
+@Slf4j
+@Service
+public class Auth0Util {
+
+    @Value("${spring.security.oauth2.client.registration.auth0.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.auth0.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.auth0-mgm.client-id}")
+    private String mgmClientId;
+    @Value("${spring.security.oauth2.client.registration.auth0-mgm.client-secret}")
+    private String mgmClientSecret;
+    @Value("${oauth2-info.auth0-mgm.audience}")
+    private String mgmAudience;
+
+
+
+
+    @Cacheable(value = "tokens", key = "'mgmAccessToken'")
+    public AccessTokenResponse getMGMApiAccessToken() {
+        log.debug(EXTERNAL_GET_REQUEST_WITH_CRITERIA, "Auth0 API", "get MGM Access Token");
+
+        WebClient webClient = WebClient.builder().baseUrl("https://dev-z383db7saml34grv.us.auth0.com/oauth/token").build();
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "client_credentials");
+        formData.add("client_id", mgmClientId);
+        formData.add("client_secret", mgmClientSecret);
+        formData.add("audience", mgmAudience);
+        return webClient.post()
+                .header("content-type", "application/json")
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve()
+                .bodyToMono(AccessTokenResponse.class)
+                .block();
+
+    }
+
+    @Cacheable(value = "tokens", key = "'revokedTokens'")
+    public AccessTokenAudAndJti[] getRevokedAccessTokens(String mgmAccessToken) {
+        log.debug(EXTERNAL_GET_REQUEST_WITH_CRITERIA, "Auth0 API", "get Revoked Access Token");
+
+        WebClient webClient = WebClient.builder().baseUrl("https://dev-z383db7saml34grv.us.auth0.com/api/v2/blacklists/tokens").build();
+        return webClient.get()
+                .header("Authorization", "Bearer " + mgmAccessToken)
+                .retrieve()
+                .bodyToMono(AccessTokenAudAndJti[].class)
+                .block();
+    }
+
+    public String revokeAccessToken(String mgmAccessToken, String jti) {
+        log.debug(EXTERNAL_GET_REQUEST_WITH_CRITERIA, "Auth0 API", "Revoke user Access token, jti " + jti);
+
+        WebClient webClient = WebClient.builder().baseUrl("https://dev-z383db7saml34grv.us.auth0.com/api/v2/blacklists/tokens").build();
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("aud", clientId);
+        formData.add("jti", jti);
+        return webClient.post()
+                .header("content-type", "application/json")
+                .header("Authorization", "Bearer " + mgmAccessToken)
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response -> {
+                    if (response.statusCode() == HttpStatus.BAD_REQUEST) {
+                        return Mono.error(new Auth0Exceptions.BadRequestException("Invalid request body"));
+                    } else if (response.statusCode() == HttpStatus.UNAUTHORIZED) {
+                        return Mono.error(new Auth0Exceptions.UnauthorizedAccessException("Invalid token or client is not global"));
+                    } else if (response.statusCode() == HttpStatus.FORBIDDEN) {
+                        return Mono.error(new Auth0Exceptions.ForbiddenException("Insufficient scope or cannot blacklist tokens for the specified audience"));
+                    } else if (response.statusCode() == HttpStatus.TOO_MANY_REQUESTS) {
+                        return Mono.error(new Auth0Exceptions.TooManyRequestsException("Too many requests"));
+                    } else {
+                        return Mono.error(new RuntimeException("Unknown client error"));
+                    }
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, response -> Mono.error(new RuntimeException("Server error")))
+                .bodyToMono(String.class)
+                .block();
+    }
+
+
+
+
+}
