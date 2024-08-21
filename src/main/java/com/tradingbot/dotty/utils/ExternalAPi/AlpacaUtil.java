b5b004326cda47b5e711d49a18ced85040dcb95c
Mina Habib <mhcsc01@gmail.com>
2024-08-21 18:09:50 -0400

    Refactor - webClient Endpoints.

--- src/main/java/com/tradingbot/dotty/utils/ExternalAPi/AlpacaUtil.java
+++ src/main/java/com/tradingbot/dotty/utils/ExternalAPi/AlpacaUtil.java
@@ -1,9 +1,23 @@
 package com.tradingbot.dotty.utils.ExternalAPi;
 
+import com.tradingbot.dotty.models.dto.UserConfigurationDTO;
+import com.tradingbot.dotty.models.dto.UsersDTO;
+import com.tradingbot.dotty.service.UserConfigurationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class AlpacaUtil {
+
+    @Autowired
+    private WebClient webClientAlpaca;
+
+    @Autowired
+    private WebClient webClientAlpacaPaper;
+
 }
